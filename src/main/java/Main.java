da4666d7e577e0e68d07bee5053bd3013bd23ee8
Pavlo I <ivchenko.pa@gmail.com>
2026-09-22 00:10:35 +0200

    Initial commit: - add Maven project - update pom.xml file - declare maven-surfire-plugin - add Main class - add main method - add sum method - add sum method test

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    static void main(String[] args) {
+        System.out.println("PI: This is an output from main method.");
+    }
+
+    public static int sum(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void sum_result6_when2and4() {
+        int input_a = 2;
+        int input_b = 4;
+
+        int expected = 6;
+        int actual = Main.sum(input_a,input_b);
+        assertEquals(expected, actual);
+    }
+}
