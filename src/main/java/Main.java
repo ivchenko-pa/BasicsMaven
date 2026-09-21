13bd8f55f466219eddb16ff64eb5b7bb289ae6e7
Pavlo I <ivchenko.pa@gmail.com>
2026-09-22 00:21:18 +0200

    Some minor canges in output text

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     static void main(String[] args) {
-        System.out.println("PI: This is an output from main method.");
+        System.out.println("PI: This is an output from main method!");
     }
 
     public static int sum(int a, int b){
+        System.out.println("We are in sum method");
         return a+b;
     }
 }
